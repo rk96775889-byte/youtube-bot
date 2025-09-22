a097f2cb6e90abdfdf820ee085b571eab7afde27
rk96775889-byte <rk96775889@gmail.com>
2025-09-22 09:45:28 +0530

    Add files via upload
    upload main files

---
+++ runner.py
@@ -0,0 +1,9 @@
+import subprocess
+
+# Run trading script
+subprocess.Popen(["python", "trading.py"])
+
+# Run main.py script
+subprocess.Popen(["python", "main.py"])
+
+print("✅ Dono scripts start ho gayi hain!")
